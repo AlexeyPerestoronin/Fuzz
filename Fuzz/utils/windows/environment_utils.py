3cab49d228c9c88962ce72890b8bebff31aa97fc
Alexey Perestoronin <Alexey.Perestoronin@yandex.ru>
2025-11-28 05:03:10 -0800

    add extend_path

--- Fuzz/utils/windows/environment_utils.py
+++ Fuzz/utils/windows/environment_utils.py
@@ -1,3 +1,18 @@
+def extend_path(paths: list):
+    context = {}
+
+    def setup_context(env_name, env_value):
+        context[env_name] = env_value
+
+    path_extensions = [
+        *paths,
+        # add here env-variables should be shared on each invoke-task on windows
+    ]
+    path_string = ";".join(path_extensions) + ";%PATH%"
+    setup_context("PATH", path_string)
+
+    return context
+
 def activate_VS2019_environment():
     return [
         "call",
